161ad8fa8923afe10edcd61763287417345eb47d
Josias Silva <josias.silva@nagem.com.br>
2021-10-06 10:41:23 -0300

    aula 08

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -3,8 +3,9 @@ package academy.devdojo.springboot2.controller;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import academy.devdojo.springboot2.domain.Anime;
@@ -14,7 +15,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
-@RequestMapping("/anime")
 @Log4j2
 @RequiredArgsConstructor
 public class AnimeController {
@@ -22,9 +22,19 @@ public class AnimeController {
 	private final DateUtil dateUtil;
 	private final AnimeService animeService;
 	
-	@GetMapping("/list")
-	public List<Anime> list() {
+	@GetMapping("/anime/list")
+	public ResponseEntity<List<Anime>> list() {
 		log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-		return animeService.listAll();
+		return ResponseEntity.ok(animeService.listAll());
+	}
+	
+	@GetMapping("/anime/id/{id}")
+	public Anime id(@PathVariable long id) {
+		return animeService.findById(id);
+	}
+	
+	@GetMapping("/anime/name/{name}")
+	public Anime name(@PathVariable String name) {
+		return animeService.findByName(name);
 	}
 }
--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -2,15 +2,32 @@ package academy.devdojo.springboot2.service;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import academy.devdojo.springboot2.domain.Anime;
 
 @Service
 public class AnimeService {
 //  private final AnimeRepository animeRepository;
+	private List<Anime> animes = List.of(new Anime(1L,"DBZ"), new Anime(2L,"Berserk"));
 	
 	public List<Anime> listAll() {
-		return List.of(new Anime(1L,"DBZ"), new Anime(2L,"Berserk"));
+		return animes;
+	}
+	
+	public Anime findById(long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+    }
+	
+	public Anime findByName(String name) {
+		return animes.stream()
+                .filter(anime -> anime.getName().equals(name))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
 	}
 }
