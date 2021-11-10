258e9987ed49d5fad287954528b6526e8bdb65e9
Jvnyor <josias100junior@gmail.com>
2021-11-10 15:14:00 -0300

    aula 44

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -58,7 +58,6 @@ public class AnimeController {
 	}
 	
 	@GetMapping("/by-id/{id}")
-	@PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Anime> findByIdAuthenticationPrincipal(@PathVariable long id,
                                                                  @AuthenticationPrincipal UserDetails userDetails) {
         log.info(userDetails);
@@ -72,20 +71,19 @@ public class AnimeController {
 	}
 	
 	@PostMapping
-	@PreAuthorize("hasRole('ADMIN')")
 	@Operation(description = "Add animes")
 	public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody animePostRequestBody) {
 		return new ResponseEntity<>(animeService.save(animePostRequestBody), HttpStatus.CREATED);
 	}
 	
-	@DeleteMapping("/delete/{id}")
+	@DeleteMapping("/admin/delete/{id}")
 	@Operation(description = "Delete a anime by id")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		animeService.delete(id);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 	
-	@PutMapping("/replace")
+	@PutMapping("/admin/replace")
 	@Operation(description = "Delete a anime by id")
 	public ResponseEntity<Void> replace(@RequestBody AnimePutRequestBody animePutRequestBody) {
 		animeService.replace(animePutRequestBody);
