901086ac7c80dbede3324f982f93c4791e600ca0
Jvnyor <josias100junior@gmail.com>
2021-11-11 15:33:09 -0300

    aula 48 (erros para verificar em ambiente de casa)

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -26,6 +26,8 @@ import academy.devdojo.springboot2.requests.AnimePostRequestBody;
 import academy.devdojo.springboot2.requests.AnimePutRequestBody;
 import academy.devdojo.springboot2.service.AnimeService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -40,8 +42,9 @@ public class AnimeController {
 	private final AnimeService animeService;
 	
 	@GetMapping
-	@Operation(description = "List of all animes in pages")
-	public ResponseEntity<Page<Anime>> list(Pageable pageable) {
+	@Operation(summary = "List all animes paginated", description = "The default size is 20, use the parameter size to change the default value",
+	tags = {"anime"})
+	public ResponseEntity<Page<Anime>> list(@ParameterObject Pageable pageable) {
 		return ResponseEntity.ok(animeService.listAll(pageable));
 	}
 	
@@ -77,6 +80,10 @@ public class AnimeController {
 	}
 	
 	@DeleteMapping("/admin/{id}")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "204", description = "Successful Operation"),
+			@ApiResponse(responseCode = "400", description = "When Anime Does Exist in The Database")
+	})
 	@Operation(description = "Delete a anime by id")
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		animeService.delete(id);
